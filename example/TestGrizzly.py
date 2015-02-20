b70feea7ebe3a7b3aa61e6964b6e490c60a42632
Douglas Hutchings <11douglash@berkeley.edu>
2015-02-19 16:17:23 -0800

    Grizzly test script that does not depend on joystick.
    Forward, backward, then stop

---
+++ example/TestGrizzly.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from grizzly import *
+from time import sleep
+#from xbox_read import event_stream
+
+g = Grizzly()
+g.set_mode(ControlMode.NO_PID, DriveMode.DRIVE_BRAKE)
+g.limit_acceleration(20)
+g.limit_current(5)
+print 'Forward'
+g.set_target(100)
+sleep(1)
+print 'Backward'
+g.set_target(-100)
+sleep(1)
+print 'Done'
+g.set_target(0)
+'''
+inputs = event_stream(4000)
+for event in inputs:
+    if event.key == "Y2":
+        throttle = int(event.value) / 328
+        g.set_target(throttle)
+    if event.key == "A":
+        print("Current: " + str(g.read_motor_current()) + "Encoder: " + str(g.read_encoder()))
+'''
+
+# Appendix: Keys
+# --------------
+# Key:                      Range of values:
+# X1                        -32768 : 32767
+# Y1                        -32768 : 32767
+# X2                        -32768 : 32767
+# Y2                        -32768 : 32767
+# du                        0, 1
+# dd                        0, 1
+# dl                        0, 1
+# dr                        0, 1
+# back                      0, 1
+# guide                     0, 1
+# start                     0, 1
+# TL                        0, 1
+# TR                        0, 1
+# A                         0, 1
+# B                         0, 1
+# X                         0, 1
+# Y                         0, 1
+# LB                        0, 1
+# RB                        0, 1
+# LT                        0 : 255
+# RT                        0 : 255
